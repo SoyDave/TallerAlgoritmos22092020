99124866144ccb93a71362370535ffd0483cb564
Juan D Ortiz C <soydavex@gmail.com>
2020-09-22 14:55:25 -0500

    Ejercicio dos realizado

---
+++ ejercicioDos.js
@@ -0,0 +1,14 @@
+//Algoritmo que lea dos números, calculando y escribiendo el valor de su suma, resta, producto y división
+
+    let numeroUno = 50;
+    let numeroDos = 50;
+
+    let suma = numeroUno + numeroDos;
+    let resta = numeroUno - numeroDos;
+    let producto = numeroUno * numeroDos;
+    let division = numeroUno / numeroDos;
+
+    console.log ("suma =" + "" + suma);
+    console.log ("resta =" + "" + resta);
+    console.log ("producto =" + "" + producto);
+    console.log ("division =" + "" + division);
--- ejercicioUno.js
+++ ejercicioUno.js
@@ -1,7 +1,9 @@
+//Dadas dos variables numéricas A y B, que el usuario debe teclear, se pide realizar un algoritmo que intercambie 
+//los valores de ambas variables y muestre cuanto valen al final las dos variables (recuerda la asignación)
 
-    var a = 1;
-    var b = 2;
-    var c = a;
+    let a = 1;
+    let b = 2;
+    let c = a;
 
     a = b;
     b = c;
