dd4fafa2a345098a8f9c37fcef23a6dcc879d640
Juan D Ortiz C <soydavex@gmail.com>
2020-09-22 15:03:10 -0500

    Ejercicio tres realizado

---
+++ ejercicioTres.js
@@ -0,0 +1,15 @@
+//Algoritmo que lea dos números y nos diga cual de ellos es mayor o bien si son iguales 
+//(recuerda usar la estructura condicional SI)
+
+    let numeroUno = 10;
+    let numeroDos = 10;
+
+    if (numeroUno > numeroDos){
+        console.log ("numero uno es mayor:" + " " + numeroUno + " " + ">" + " " + numeroDos);
+    }
+    else if (numeroDos > numeroUno){
+        console.log ("numero dos es mayor:" + " " + numeroUno + " " + "<" + " " + numeroDos);
+    }
+    else{
+        console.log ("ambos numeros son iguales:" + " " + numeroUno + " " + "=" + " " + numeroDos);
+    };
