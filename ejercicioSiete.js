b951684e022f6e3464d77a668b20f2f8cf3ff7be
Juan D Ortiz C <soydavex@gmail.com>
2020-09-25 18:21:08 -0500

    Ejercicio cuatro en sus dos formas

---
+++ ejercicioSiete.js
@@ -0,0 +1,4 @@
+//Un colegio desea saber qué porcentaje de niños y qué porcentaje de niñas hay en el curso actual. 
+//Diseñar un algoritmo para este propósito (recuerda que para calcular el porcentaje puedes hacer una regla de 3).
+
+    
