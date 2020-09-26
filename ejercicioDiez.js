67d23d1b37956b1d59ed9c46838634c68000e11d
Juan D Ortiz C <soydavex@gmail.com>
2020-09-25 20:00:26 -0500

    Ejercicio diez realizado

---
+++ ejercicioDiez.js
@@ -0,0 +1,37 @@
+//Modificar el algoritmo anterior, de forma que si se teclea un cero, 
+//se vuelva a pedir el número por teclado (así hasta que se teclee un número mayor que cero) (recuerda la estructura mientras)
+
+        let numero = parseInt(0);
+
+        if (numero <= 0) {
+        console.log("el numero no puede ser menor o igual a 0");
+        }else if(numero % 2 == 0){
+                console.log(numero + " es par");
+            }
+            else{
+                console.log(numero + " es impar");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
