4c88377f2765897d121a0f69947dc96cd30872f7
Juan D Ortiz C <soydavex@gmail.com>
2020-09-27 18:29:53 -0500

    Ejercicio doce realizado

--- ejercicioDiez.js
+++ ejercicioDiez.js
@@ -1,5 +1,5 @@
 //Modificar el algoritmo anterior, de forma que si se teclea un cero, 
-//se vuelva a pedir el número por teclado (así hasta que se teclee un número mayor que cero) (recuerda la estructura mientras)
+//se vuelva a pedir el número por teclado (así hasta que se teclee un número mayor que cero) 
 
         let numero = parseInt(0);
 
---
+++ ejercicioDoce.js
@@ -0,0 +1,12 @@
+//Desarrollar un algoritmo que nos calcule el cuadrado de los 9 primeros números naturales.
+
+let numeroInicial = 0, numeroFinal = 8;
+
+while (numeroInicial <= numeroFinal) {
+    let cuadrado;    
+    numeroInicial += 1;
+    console.log(cuadrado = Math.pow(numeroInicial, 2));
+}
+
+
+
